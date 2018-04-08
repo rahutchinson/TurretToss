d40dac386153388372d6ffb68d03ef67514d342a
Ryan Hutchinson <ryanahutchinson@gmail.com>
2018-04-08 18:05:02 -0500

    Update Turret.py

--- Turret.py
+++ Turret.py
@@ -54,3 +54,19 @@ def loop():
     time.sleep(10)
     resetMotors()
 loop()
+
+import serial
+import time
+
+def check_shot():
+    ser = serial.Serial('/dev/ttyACM0',9600)
+    s = None
+    ser.write(bytes(b'1'))
+	time.sleep(3)
+	try:
+		s = str(int (ser.readline()))	
+	except:
+		pass
+	if(s):
+		return(1)
+	return(0)
